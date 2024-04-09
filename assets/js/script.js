365d003e37c6b9fb660029712c146e332022cbc7
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-09 22:45:24 +0200

    Fixed: content {{ }} - MILESTONE 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            mails: [],
-            
+            mails: [],        
         };
     },
     methods: {
@@ -20,7 +19,7 @@ createApp({
             // mail random ottenuta da API
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                const serverMail = response.data;
+                const serverMail = response.data;    
                 //pusho mail trovata in Api nell'array vuoto creato.
                 this.mails.push(serverMail.response);              
             });
