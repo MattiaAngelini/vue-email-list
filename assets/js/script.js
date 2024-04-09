055b791e18f3ced861bf1cf5f1ef0e701e67ca1b
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-09 17:50:27 +0200

    Add: print mails on dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,31 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            prova: 0,
+            mails: [],
+            
         };
     },
     methods: {
-     
+        get10Mails() {
+            for(i = 0; i < 10; i++){
+                this.getMailFromApi();
+            }
+        },
+      
+        getMailFromApi() {
+            // Chiamo l'api che genera una mail
+            // quando otteniamo la risposta: popoliamo la variabile mails con
+            // mail random ottenuta da API
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const serverMail = response.data;
+                //pusho mail trovata in Api nell'array vuoto creato.
+                this.mails.push(serverMail.response);              
+            });
+        },
+        
     },
     mounted() {
-        
+        this.get10Mails(); 
     },
 }).mount('#app');
